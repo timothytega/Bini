12e2477909bb257e85f050d23e297ddb0fbd2b6c
timothytega <45305218+timothytega@users.noreply.github.com>
2020-07-21 07:23:27 -0700

    Add files via upload

---
+++ list..1.py
@@ -0,0 +1,18 @@
+L=[]
+L2=[]
+L3=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(50):
+    L.append(i)
+print(L)
+for i in L:
+    L2.append(i**2)
+print(L2)
+for i in range(1,26):
+    for J in L3:
+        x=i*J
+        print(x)
+
+
+
+
+
